e12330cd2f93b99a2f6c00fa4bd18f48d5a8e58a
zhangbaiyi <55794640+zhangbaiyi@users.noreply.github.com>
2020-06-10 22:18:47 +0800

    luan fei wen ti

--- Classes/SpriteMap.cpp
+++ Classes/SpriteMap.cpp
@@ -144,6 +144,7 @@ void SpriteMap::swap(int staPosition, int endPosition)
 	if (dis != 1 && dis != 8)
 		return;
 
+	isAction = true;
 	float time = 0.2;
 	auto staPtr = map[staPosition];
 	auto endPtr = map[endPosition];
@@ -166,8 +167,8 @@ void SpriteMap::swap(int staPosition, int endPosition)
 		remove(endPosition);
 		add(staPosition, staPtr);
 		add(endPosition, endPtr);
-		staPtr->runAction(toEnd->clone());
-		endPtr->runAction(toSta->clone());
+		staPtr->runAction(cocos2d::Sequence::create(toEnd->clone(), toSta->clone(), nullptr));
+		endPtr->runAction(cocos2d::Sequence::create(toSta->clone(), toEnd->clone(), nullptr));
 	}
 }
 
